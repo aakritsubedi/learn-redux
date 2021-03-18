56b8f56ce6cd34a6db88a095423612a4719ea1ed
Aakrit Subedi <aakritsubedi@pop-os.localdomain>
2021-03-18 21:36:47 +0545

    Redux Basics

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Learning Redux 
+
+- Basic Redux
+```javascript
+import { createStore } from 'redux';
+
+// Store: Globalized State
+// Action
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// Display it in console
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch
+store.dispatch(increment());
+store.dispatch(decrement());
+```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import "./assets/css/App.css";
+import { createStore } from 'redux';
+
+import './assets/css/App.css';
+
+// Store: Globalized State
+
+// Action
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// Display it in console
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch
+store.dispatch(increment());
+store.dispatch(decrement());
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
